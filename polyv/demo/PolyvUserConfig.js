7fa43c46caace908bb231beb9dcd6456a206fc13
PYFer <pengyunfei@polyv.net>
2022-10-21 09:31:18 +0800

    no message

--- polyv/demo/PolyvUserConfig.js
+++ polyv/demo/PolyvUserConfig.js
@@ -8,10 +8,10 @@ const PolyvUserConfig = {
     inputVid: "417cfa5cc19402e5d97de29a2ae05eb4_4",
 
     // 输入框默认账户
-    inputAppId: "fhtw392viy",
-    inputUserId: "417cfa5cc1",
-    inputChannelId:"2091125",
-    inputAppSecret:"07a4b8d7c3484b61bde9e1327df80c74"
+    inputAppId: "",
+    inputUserId: "",
+    inputChannelId:"",
+    inputAppSecret:""
   }
 };
 module.exports = PolyvUserConfig;
